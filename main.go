a8bcf2a41035e4b67b5d98507cd56bcc1ce5de3d
RogueBit <r0gu3b17@gmail.com>
2022-08-11 07:31:57 -0600

    minor updates

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"image"
 	"image/jpeg"
@@ -12,46 +13,73 @@ func init() {
 }
 
 func main() {
-	imgfile, err := os.Open("./img.jpg")
-
+	img, err := openImage("img.jpg")
 	if err != nil {
 		fmt.Println("img.jpg file not found!")
 		os.Exit(1)
 	}
+	fmt.Printf("imgfile: %T\n", img)
+	size := img.Bounds().Size()
+	fmt.Printf("size: %d\n", size)
+	fmt.Printf("size type: %T\n", size)
+	fmt.Printf("Width X: %d\n", size.X)
+	fmt.Printf("Height Y: %d\n", size.Y)
+	imgRect := img.Bounds()
+	fmt.Printf("Rect type: %T\n", imgRect)
+	fmt.Println(imgRect)
+	fmt.Println(imgRect.Bounds().Min.X)
+	tpixel := img.At(0, 0)
+	fmt.Printf("tpixel type: %T\n", tpixel)
+	fmt.Println(tpixel.RGBA())
 
-	defer imgfile.Close()
+	// loop over image, let's do something real...
+	for y := imgRect.Min.Y; y < imgRect.Max.Y; y++ {
+		for x := imgRect.Min.X; x < imgRect.Max.X; x++ {
+			pixelValue := rgbaToPixel(img.At(x, y).RGBA())
+			pixelEven := pixelIsEven(pixelValue.R, pixelValue.G, pixelValue.B)
+			if pixelEven {
+				fmt.Printf("[%d,%d:%t]:[White]\n", x, y, pixelEven)
+			} else {
+				fmt.Printf("[%d,%d:%t]:[Black]\n", x, y, pixelEven)
+			}
 
-	imgCfg, _, err := image.DecodeConfig(imgfile)
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	width := imgCfg.Width
-	height := imgCfg.Height
-
-	fmt.Println("Width : ", width)
-	fmt.Println("Height : ", height)
-
-	imgfile.Seek(0, 0)
-
-	// get the image
-	img, _, err := image.Decode(imgfile)
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	fmt.Println("Pixel map:")
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			rgbaPixel := rgbaToPixel(img.At(x, y).RGBA())
-
-			fmt.Printf("[X : %d Y : %v] R : %v, G : %v, B : %v, A : %v  \n", x, y, rgbaPixel.R, rgbaPixel.G, rgbaPixel.B, rgbaPixel.A)
 		}
 	}
+}
+
+func openImage(path string) (image.Image, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer f.Close()
+	fi, _ := f.Stat()
+
+	fmt.Println(fi.Name())
+
+	img, format, err := image.Decode(f)
+	if err != nil {
+		fmt.Println("Decoding error:", err.Error())
+		return nil, err
+	}
+	if format != "jpeg" {
+		fmt.Println("image format is not jpeg")
+		return nil, errors.New("")
+	}
+	return img, nil
+}
+
+func pixelIsEven(r int, g int, b int) bool {
+	pixelSum := r + g + b
+	//fmt.Println(pixelSum)
+	//fmt.Println(pixelSum % 2)
+	if pixelSum%2 == 0 {
+		return true
+	} else {
+		return false
+	}
+
 }
 
 // img.At(x, y).RGBA() returns four uint32 values; we want a Pixel
